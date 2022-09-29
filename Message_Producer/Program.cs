ed5c9502df50151414cc474dfeeeb5711ca9b9ea
Thomas Leo Blok <37294981+thomasleoblok@users.noreply.github.com>
2022-09-29 10:24:44 +0200

    Add project files.

---
+++ Message_Producer/Program.cs
@@ -0,0 +1,48 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    while (true)
+    {
+
+
+        channel.ExchangeDeclare(exchange: "topic_logs",
+                                type: "topic");
+
+
+        Console.WriteLine("Indtast besked");
+        string message = Console.ReadLine();
+
+        string routingKey = "anonymus";
+
+        if (message.ToLower().Contains("gadget"))
+            routingKey = "gadget.info";
+        else if (message.ToLower().Contains("widget"))
+            routingKey = "widget.info";
+
+
+        var body = Encoding.UTF8.GetBytes(message);
+        channel.BasicPublish(exchange: "topic_logs",
+                             routingKey: routingKey,
+                             basicProperties: null,
+                             body: body);
+        //PrintToScreen($" [x] Sent '{routingKey}':'{message}'");
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            var routingKey = ea.RoutingKey;
+
+
+        };
+        channel.BasicConsume(queue: channel.QueueDeclare().QueueName,
+                             autoAck: true,
+                             consumer: consumer);
+    }
+}
---
+++ RabbitMQ_Content_Based_Routing/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.ExchangeDeclare(exchange: "topic_logs", type: "topic");
+    var queueName = channel.QueueDeclare().QueueName;
+
+
+    channel.QueueBind(queue: queueName,
+          exchange: "topic_logs",
+                      routingKey: "widget.info");
+
+    Console.WriteLine("widget.info");
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        var routingKey = ea.RoutingKey;
+        Console.WriteLine(" [x] Received '{0}':'{1}'",
+                          routingKey,
+                          message);
+    };
+    channel.BasicConsume(queue: queueName,
+                         autoAck: true,
+                         consumer: consumer);
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
